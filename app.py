f5252d39c777cf4524f4bd300f31467b2fca09ec
Ritesh Kumar <120010664+Ritesh-0001@users.noreply.github.com>
2025-04-17 14:20:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def guess():
         return jsonify({'message': f'Correct! The number was {number}. Starting a new game...', 'correct': True})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=8080,debug=True)
